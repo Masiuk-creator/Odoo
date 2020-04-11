4a35d9dea844d46754af6cf56799317864eb7bdf
Masiuk-creator <nataliamasiuk1973@gmail.com>
2020-04-11 11:52:56 +0300

    parsing currency

--- models/res_currency_rate_provider.py
+++ models/res_currency_rate_provider.py
@@ -1,12 +1,37 @@
-import json,urllib.request
-
-NBU = urllib.request.urlopen("https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json").read()
-output = json.loads(NBU)
-z=output[0:58]
-print (z)
+import json, urllib.request
 
 
-PrivatBank = urllib.request.urlopen("https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5").read()
-output2 = json.loads(PrivatBank)
-k=output2[0:3]
-print (k)
+class Parser():
+    def __init__(self):
+        valuta_NBU = []
+        valuta_PrivatBank = []
+
+    def Get_Data_NBU(self):
+        NBU = urllib.request.urlopen("https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json").read()
+        output = json.loads(NBU)
+        return output
+
+    def Get_Data_PrivatBank(self):
+        PrivatBank = urllib.request.urlopen("https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5").read()
+        output2 = json.loads(PrivatBank)
+        return output2
+
+    def foreach_valuta_PrivatBank():
+        for valuta in Get_Data_PrivatBank():
+            if valuta['ccy'] == 'EUR':
+                valuta_NBU['eur_privatbank'] = valuta['buy']
+            if valuta['ccy'] == 'USD':
+                valuta_NBU['usd_privatbank'] = valuta['buy']
+            if valuta['ccy'] == 'RUR':
+                valuta_NBU['rur_privatbank'] = valuta['buy']
+        print(valuta_PrivatBank[])
+
+    def foreach_valute_NBU():
+        for valuta in Get_Data_NBU():
+            if valuta['cc'] == 'EUR':
+                valuta_NBU['eur_nbu'] = valuta[rate]
+            if valuta['cc'] == 'USD':
+                valuta_NBU['usd_nbu'] = valuta[rate]
+            if valuta['cc'] == 'RUB':
+                valuta_NBU['rub_nbu'] = valuta[rate]
+        print(valuta_NBU[])
